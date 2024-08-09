5209547d49aa9c60a3123c55aa5001663bfe0af2
KimPuro <puro@protonmail.ch>
2024-08-09 13:13:45 +0900

    fix : project 페이지 크기 수정

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,31 +1,37 @@
 import NoProject from '@/components/project/NoProject'
-import ProjectList from '@/components/project/ProjectList';
+import ProjectList from '@/components/project/ProjectList'
 import Sidebar from '@/components/project/Sidebar'
-import { fetchProjectsByUserId } from '@/services/projectService';
-import { cookies } from 'next/headers';
-
-
+import { fetchProjectsByUserId } from '@/services/projectService'
+import { cookies } from 'next/headers'
+import Header from '@/components/common/Header'
+import Footer from '@/components/common/Footer'
 
 export default async function Page() {
-  const userEmail = "testUser@naver.com";
-  const username = cookies().get('username');
+  const userEmail = 'testUser@naver.com'
+  const username = cookies().get('username')
 
-  const projects = await fetchProjectsByUserId(userEmail);
+  const projects = await fetchProjectsByUserId(userEmail)
 
-  if (!projects|| projects.length === 0) {
+  if (!projects || projects.length === 0) {
     return (
-      <div className="flex">
-        <Sidebar />
-        <NoProject />
-      </div>
-    );
+      <>
+        <Header />
+        <div className="flex pt-10">
+          <Sidebar />
+          <NoProject />
+        </div>
+        <Footer />
+      </>
+    )
   }
   return (
-    <main>
+    <>
+      <Header />
       <div className="flex">
         <Sidebar />
         <ProjectList projects={projects} />
       </div>
-    </main>
+      <Footer />
+    </>
   )
 }
