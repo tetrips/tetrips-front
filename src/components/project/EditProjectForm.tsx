83b86c642fa1e1539be3e295d8854df97b5c2c14
chldmsqls34 <chldmsqls34@gmail.com>
2024-08-09 14:41:21 +0900

    fix: 여행 계획 편집 컴포넌트 화면 비율

--- src/components/project/EditProjectForm.tsx
+++ src/components/project/EditProjectForm.tsx
@@ -15,17 +15,19 @@ export default function EditProjectForm({ projectData, placesData,userData }: Ed
 
 
   return (
-    <div className="flex flex-1 overflow-hidden">
-      <div className="w-1/3 h-full overflow-y-auto no-scrollbar border-r">
-        <ItineraryBox
-          project={projectData}
-          initialPlaces={placesData}
-        />
+    <div className="flex flex-col xl:flex-row h-screen overflow-hidden">
+      <div className="flex flex-col lg:flex-row xl:flex-row h-[70%] lg:h-2/3 xl:h-full xl:w-5/6">
+        <div className="w-full lg:w-1/2 xl:w-2/5 h-1/2 lg:h-full overflow-y-auto no-scrollbar border-b lg:border-b-0 lg:border-r">
+          <ItineraryBox
+            project={projectData}
+            initialPlaces={placesData}
+          />
+        </div>
+        <div className="w-full lg:w-1/2 xl:w-3/5 h-1/2 lg:h-full">
+          <NaverMap project={projectData} />
+        </div>
       </div>
-      <div className="w-1/2 h-full">
-        <NaverMap project={projectData} />
-      </div>
-      <div className="w-1/6 h-full overflow-y-auto no-scrollbar">
+      <div className="w-full h-[30%] lg:h-1/3 xl:h-full xl:w-1/6 overflow-y-auto no-scrollbar border-t lg:border-t-0 xl:border-l">
         <ChatBox project={projectData} userData={userData}/>
       </div>
     </div>
