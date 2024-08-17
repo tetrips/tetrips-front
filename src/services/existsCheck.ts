cce6128232635cc000aca63240bf71af709eea77
KimPuro <puro@protonmail.ch>
2024-08-17 21:04:51 +0900

    test : api url 수정

--- src/services/existsCheck.ts
+++ src/services/existsCheck.ts
@@ -1,36 +1,35 @@
-export async function existsEmailCheck(email:string){
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/exists-email?email=${email}`, {
-  });
-  console.log(response);
-  if(response.status === 409){
-    return 'fail';
-  }
-  else if(response.status === 200){
-    return 'success';
-  }
-  else {
-    return 'error';
+export async function existsEmailCheck(email: string) {
+  const response = await fetch(
+    `${process.env.API_BASE_URL}/user/exists-email?email=${email}`,
+    {},
+  )
+  console.log(response)
+  if (response.status === 409) {
+    return 'fail'
+  } else if (response.status === 200) {
+    return 'success'
+  } else {
+    return 'error'
   }
 }
-export async function existsNicknameCheck(nickname : string){
-  const encoder = new TextEncoder();
+export async function existsNicknameCheck(nickname: string) {
+  const encoder = new TextEncoder()
   const encodedNickname = Array.from(encoder.encode(nickname))
-    .map(byte => '%' + byte.toString(16).padStart(2, '0'))
-    .join('');
-// utf-8로 인코딩 후 url-safe 인코딩
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/exists-nickname?nickname=${encodedNickname}`, {
-  });
-  console.log(response);
-  if(response.status === 409){
-    return 'fail';
-  }
-  else if(response.status === 200){
-    return 'success';
-  }
-  else if(response.status === 400){
-    return 'not-supported';
-  }
-  else {
-    return 'error';
+    .map((byte) => '%' + byte.toString(16).padStart(2, '0'))
+    .join('')
+  // utf-8로 인코딩 후 url-safe 인코딩
+  const response = await fetch(
+    `${process.env.API_BASE_URL}/user/exists-nickname?nickname=${encodedNickname}`,
+    {},
+  )
+  console.log(response)
+  if (response.status === 409) {
+    return 'fail'
+  } else if (response.status === 200) {
+    return 'success'
+  } else if (response.status === 400) {
+    return 'not-supported'
+  } else {
+    return 'error'
   }
 }
--- src/services/postLogout.ts
+++ src/services/postLogout.ts
@@ -1,13 +1,13 @@
-import Cookies from 'js-cookie';
+import Cookies from 'js-cookie'
 
 export default async function postLogout() {
-  const refreshToken = Cookies.get('refreshToken');
+  const refreshToken = Cookies.get('refreshToken')
 
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ refreshToken }),
-    });
-  }
+  const response = await fetch(`${process.env.API_BASE_URL}/auth/logout`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ refreshToken }),
+  })
+}
