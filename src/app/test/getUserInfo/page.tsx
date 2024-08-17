80bbdc4fcf7189b63afb6cf0d5463ee2331fd71b
KimPuro <puro@protonmail.ch>
2024-08-18 01:18:35 +0900

    test : login fetch 쿠키 로직 수정

--- src/app/test/getUserInfo/page.tsx
+++ src/app/test/getUserInfo/page.tsx
@@ -1,7 +1,5 @@
 'use client'
-import Cookies from 'js-cookie'
 import { getUserInfo } from '@/services/getUserInfo'
-import { getAccessToken } from '@/services/getAccessToken'
 export default function Page() {
   const res = async () => {
     return await getUserInfo()
